0ea47a97c71d3bf1c926f38a8a0a02421a4e4b97
soumiya59 <soumiyaus@gmail.com>
2023-02-11 12:06:19 +0100

    cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import { useState } from "react"
+import tw from "twin.macro"
+
+const TEXTAREA = tw.textarea`
+text-xl p-2 md:p-4 border-2 focus:outline-none focus:ring-0 focus:border-orange-200 md:max-w-[50%] max-w-sm 
+` 
 
 function App() {
   const [input,setInput] = useState('') 
@@ -8,14 +13,13 @@ function App() {
     setFlip(true)
   }
   return (
-    <section className="container bg-whitish mx-auto md:py-16 ">
+    <section className="container bg-whitish mx-auto md:py-16">
 
-      <h1 className="text-center text-3xl md:text-5xl pt-5 uppercase text-redish font-bold ">mirror writing generator</h1>
-
+      <h1 className="text-center text-2xl md:text-5xl pt-5 uppercase text-redish font-bold mx-1 ">mirror writing generator</h1>
       <form className="container mx-auto md:flex lg:justify-evenly text-center py-16" >
-        <textarea name="input" value={input} cols={50} rows={9} placeholder='Enter text here' className="text-xl p-2 md:p-4 border-2 focus:outline-none focus:ring-0 focus:border-orange-200 max-w-[90%] md:max-w-[50%]" onChange={handleChange} autoFocus></textarea>
+        <TEXTAREA name="input" value={input} cols={50} rows={9} placeholder='Enter text here' onChange={handleChange} autoFocus className="mb-5 md:mb-0"></TEXTAREA>
         <br />
-        <textarea name="input" value={input} cols={50} rows={9} placeholder='mirrored text will appear here' className={flip?'text-xl fliped border-2 p-2 md:p-4 mt-10 md:mt-0 max-w-[90%] md:max-w-[50%]  focus:outline-none focus:ring-0': 'text-xl border-2 p-2 md:p-4 mt-10 md:md:mt-0 max-w-[90%] md:max-w-[50%] focus:outline-none focus:ring-0'} >{input}</textarea>
+        <TEXTAREA name="input" value={input} cols={50} rows={9} placeholder='mirrored text will appear here' className={flip ? 'fliped' : ''} >{input}</TEXTAREA>
       </form>
 
     </section>
